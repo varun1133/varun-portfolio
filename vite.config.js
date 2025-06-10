39db281a5bc7328e0828764625436ce6da9dd920
varun1133 <101275380+varun1133@users.noreply.github.com>
2025-06-10 20:53:21 +0530

    resolved_pdf_view

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/VARUN-PORTFOLIO/',
+  base: '/varun-portfolio/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
